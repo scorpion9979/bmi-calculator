f5519e4f30d075b2d62976e7af5c6ea28456f3f9
scorpion9979 <ahmed_ihsan_tawfeek@yahoo.com>
2020-02-21 11:49:19 +0300

    Added basic layout

--- lib/input_page.dart
+++ lib/input_page.dart
@@ -6,6 +6,9 @@ class InputPage extends StatefulWidget {
 }
 
 class _InputPageState extends State<InputPage> {
+  static const double margin = 8;
+  static const int color = 0xFF1D1F33;
+  static const double borderRadius = 10;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -14,12 +17,73 @@ class _InputPageState extends State<InputPage> {
           child: Text('BMI CALCULATOR'),
         ),
       ),
-      body: Center(
-        child: Text('Body Text'),
+      body: Column(
+        children: <Widget>[
+          CardRow(
+            cards: [
+              Card(
+                margin: margin,
+                color: color,
+                borderRadius: borderRadius,
+              ),
+              Card(
+                margin: margin,
+                color: color,
+                borderRadius: borderRadius,
+              ),
+            ],
+          ),
+          Card(
+            margin: margin,
+            color: color,
+            borderRadius: borderRadius,
+          ),
+          CardRow(
+            cards: [
+              Card(
+                margin: margin,
+                color: color,
+                borderRadius: borderRadius,
+              ),
+              Card(
+                margin: margin,
+                color: color,
+                borderRadius: borderRadius,
+              ),
+            ],
+          ),
+        ],
       ),
-      floatingActionButton: FloatingActionButton(
-        child: Icon(Icons.add),
+    );
+  }
+}
+
+class Card extends StatelessWidget {
+  final double margin;
+  final int color;
+  final double borderRadius;
+  Card({this.margin, this.color, this.borderRadius});
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Container(
+        margin: EdgeInsets.all(this.margin),
+        decoration: BoxDecoration(
+          color: Color(this.color),
+          borderRadius: BorderRadius.circular(this.borderRadius),
+        ),
       ),
+    );
+  }
+}
+
+class CardRow extends StatelessWidget {
+  final List<Card> cards;
+  CardRow({this.cards});
+  @override
+  Widget build(BuildContext context) {
+    return Expanded(
+      child: Row(children: this.cards),
     );
   }
 }
